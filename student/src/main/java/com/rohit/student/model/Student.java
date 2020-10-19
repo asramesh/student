dd9208c7bc417723d67f05bcffddc6fae026ce9e
asramesh <70964502+asramesh@users.noreply.github.com>
2020-10-19 16:16:29 -0400

    Add files via upload
    initial commit

---
+++ student/src/main/java/com/rohit/student/model/Student.java
@@ -0,0 +1,75 @@
+package com.rohit.student.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "student", catalog = "mydb")
+public class Student {
+    @Id
+    int studentId;
+    String firstName;
+    String lastName;
+    @Column(name = "middleInitial")
+    String middleInitial = "N/A";
+    int age;
+    int grade;
+    int teacherId;
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+}
